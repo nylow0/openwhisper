b2e19d727a59ad4a17c0c3def431db1e81c9ea1e
Dany <ddanechka97@gmail.com>
2026-05-14 15:58:40 +0200

    Move ASR assets out of app repo

--- asr/scripts/parakeet_batch_spike.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility shim for the ASR core Parakeet batch spike."""
-
-from openwhisper_asr.engines.parakeet_batch import main
-
-if __name__ == "__main__":
-    raise SystemExit(main())
